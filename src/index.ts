6b576cb78cdc8df5882a051a836767e9eec286f7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-10 00:26:14 +0100

    basic implementation of the sorting algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('654');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    //sorting logic
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 5, -3, 0]);
