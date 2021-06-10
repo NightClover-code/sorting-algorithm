753d1c17e13f9973752e059a2ca0ef711ff5a6d0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-10 14:00:11 +0100

    huge refactor around SorterStarter

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,94 @@
 //importing utils
-const usePrompt = require('prompt-sync')();
 const Radio = require('prompt-radio');
+const usePrompt = require('prompt-sync')();
 //importing classes
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const startSorting = () => {
-  let dataStructure: string = 'Array of Numbers';
+interface Print {
+  dataStructure: NumbersCollection | CharactersCollection;
+  dataText: 'nums' | 'characters';
+  data: number[] | string;
+}
 
-  const prompt = new Radio({
+class SorterStart {
+  linkedList = new LinkedList();
+  numbers: number[] = [];
+  radioPrompt = new Radio({
     name: 'dataStructures',
     message: 'What data structure would you like to sort?',
     choices: ['Array of Numbers', 'Linked List', 'String'],
   });
 
-  prompt.ask((answer: any) => {
-    dataStructure = answer;
-    if (dataStructure === 'Array of Numbers') {
-      const numbers = [];
-      const len = usePrompt('Enter the length of the array: ');
-
-      for (let i = 0; i < len; i++) {
-        const item = usePrompt(`Enter the value of [${i}]: `);
-        numbers.push(item);
+  startSorting() {
+    this.radioPrompt.ask((answer: any) => {
+      let dataStructure = answer;
+      if (dataStructure === 'Array of Numbers') {
+        this.sortNumbers();
+      } else if (dataStructure === 'String') {
+        this.sortCharacters();
+      } else {
+        this.sortLinkedList();
       }
+    });
+  }
 
-      const numbersCollection = new NumbersCollection(numbers);
+  sortNumbers() {
+    const len = usePrompt('Enter the length of the array: ');
 
-      console.log('Previous nums: ', numbers);
-
-      numbersCollection.sort();
-
-      console.log('Sorted nums: ', numbersCollection.data);
-    } else if (dataStructure === 'String') {
-      const characters = usePrompt('Enter a string of characters: ');
-
-      const charactersCollection = new CharactersCollection(characters);
-
-      console.log('Previous characters: ', characters);
-
-      charactersCollection.sort();
-
-      console.log('Sorted Characters: ', charactersCollection.data);
-    } else {
-      const linkedList = new LinkedList();
-
-      const len = usePrompt(
-        'Enter the number of items to append to the list: '
-      );
-
-      for (let i = 0; i < len; i++) {
-        const item = usePrompt(`Enter the value ${i}: `);
-        linkedList.add(item);
-      }
-
-      linkedList.print();
-
-      linkedList.sort();
-
-      linkedList.print();
+    for (let i = 0; i < len; i++) {
+      const item = usePrompt(`Enter the value [${i}]: `);
+      this.numbers.push(item);
     }
-  });
-};
 
-startSorting();
+    const numbersCollection = new NumbersCollection(this.numbers);
+
+    numbersCollection.sort();
+    this.print({
+      data: this.numbers,
+      dataStructure: numbersCollection,
+      dataText: 'nums',
+    });
+  }
+
+  sortCharacters() {
+    const characters = usePrompt('Enter a string of characters: ');
+    const charactersCollection = new CharactersCollection(characters);
+
+    charactersCollection.sort();
+    this.print({
+      data: characters,
+      dataStructure: charactersCollection,
+      dataText: 'characters',
+    });
+  }
+
+  sortLinkedList() {
+    const len = usePrompt('Enter the number of items to append to the list: ');
+
+    for (let i = 0; i < len; i++) {
+      const item = usePrompt(`Enter the value ${i}: `);
+      this.linkedList.add(item);
+    }
+
+    this.printLinkedList();
+  }
+
+  print({ data, dataStructure, dataText }: Print) {
+    console.log(`Previous ${dataText}: `, data);
+    console.log(`Sorted ${dataText}: `, dataStructure.data);
+  }
+
+  printLinkedList() {
+    console.log(`Previous Linked List: `);
+    this.linkedList.print();
+    this.linkedList.sort();
+    console.log(`Sorted Linked List: `);
+    this.linkedList.print();
+  }
+}
+
+//start sorting
+const sorterStart = new SorterStart();
+sorterStart.startSorting();
