a549fafc2e8fa96ad86735c35fa91dad0646f120
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-10 02:51:12 +0100

    adding user feedback, refactoring to be done later

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,20 +1,14 @@
-export interface Sortable {
-  length: number;
-  compare: (leftIndex: number, rightIndex: number) => boolean;
-  swap: (leftIndex: number, rightIndex: number) => void;
-}
-
 //sorter class
 export abstract class Sorter {
   abstract compare(leftIndex: number, rightIndex: number): boolean;
   abstract swap(leftIndex: number, rightIndex: number): void;
   abstract length: number;
 
+  //sorter method
   sort(): void {
-    const { length } = this;
     //sorting logic
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
         if (this.compare(j, j + 1)) {
           this.swap(j, j + 1);
         }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,67 @@
+//importing utils
+const usePrompt = require('prompt-sync')();
+const Radio = require('prompt-radio');
 //importing classes
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([0, 5, 6, -6, 9, 5645]);
-const charactersCollection = new CharactersCollection('achrafLol');
-const linkedList = new LinkedList();
+const startSorting = () => {
+  let dataStructure: string = 'Array of Numbers';
 
-linkedList.add(500);
-linkedList.add(-62);
-linkedList.add(10);
-linkedList.add(65);
+  const prompt = new Radio({
+    name: 'dataStructures',
+    message: 'What data structure would you like to sort?',
+    choices: ['Array of Numbers', 'Linked List', 'String'],
+  });
 
-linkedList.print();
+  prompt.ask((answer: any) => {
+    dataStructure = answer;
+    if (dataStructure === 'Array of Numbers') {
+      const numbers = [];
+      const len = usePrompt('Enter the length of the array: ');
+
+      for (let i = 0; i < len; i++) {
+        const item = usePrompt(`Enter the value of [${i}]: `);
+        numbers.push(item);
+      }
+
+      const numbersCollection = new NumbersCollection(numbers);
+
+      console.log('Previous nums: ', numbers);
+
+      numbersCollection.sort();
+
+      console.log('Sorted nums: ', numbersCollection.data);
+    } else if (dataStructure === 'String') {
+      const characters = usePrompt('Enter a string of characters: ');
+
+      const charactersCollection = new CharactersCollection(characters);
+
+      console.log('Previous characters: ', characters);
+
+      charactersCollection.sort();
+
+      console.log('Sorted Characters: ', charactersCollection.data);
+    } else {
+      const linkedList = new LinkedList();
+
+      const len = usePrompt(
+        'Enter the number of items to append to the list: '
+      );
+
+      for (let i = 0; i < len; i++) {
+        const item = usePrompt(`Enter the value ${i}: `);
+        linkedList.add(item);
+      }
+
+      linkedList.print();
+
+      linkedList.sort();
+
+      linkedList.print();
+    }
+  });
+};
+
+startSorting();
